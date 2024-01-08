6eb686b1735f17800d8f9b22f1fe4b074c46cfdd
SHIVAM KASHYAP <2130103@sliet.ac.in>
2024-01-08 23:28:31 +0530

    l

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
-const BlogDetails = ({post}) => {
+const BlogDetails = ({ post }) => {
+  useEffect(() => {
+    console.log("post is - ---- ", post);
+  });
   return (
     <div>
-      <NavLink to ={`/blog/${post.id}`}>
+      <NavLink to={`/blog/${post.id}`}>
         <span>{post.title}</span>
       </NavLink>
       <p>
-        By 
+        By
         <span>{post.author}</span>on{" "}
-        <NavLink to={`/categories/${post.category.replaceAll(" ","-")}`}>
-            <span>{post.category}</span>
+        <NavLink to={`/categories/${post.category.replaceAll(" ", "-")}`}>
+          <span>{post.category}</span>
         </NavLink>
       </p>
       <p>Posted on {post.date}</p>
       <p>{post.content}</p>
       <div>
-           {post.tag.map((tag,index)=>(
-              <NavLink key ={index} to={`/tag/${tag.replaceAll(" ","-")}`}>
-                <span>{`#${tag}`}</span>
-              </NavLink>
-           ))}
+        {post.tags.map((tag, index) => (
+          <NavLink key={index} to={`/tag/${tag.replaceAll(" ", "-")}`}>
+            <span>{`#${tag}`}</span>
+          </NavLink>
+        ))}
       </div>
     </div>
   );
